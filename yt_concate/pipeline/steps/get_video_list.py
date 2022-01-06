afd11860c5eb383de2b0741a598834a7ad97d946
piggypowerycl <yichulai@gmail.com>
2022-01-06 21:40:52 +0800

    Successfully download captions and save video list to file

--- yt_concate/pipeline/steps/get_video_list.py
+++ yt_concate/pipeline/steps/get_video_list.py
@@ -7,13 +7,18 @@ from yt_concate.settings import API_KEY  # from .settings import API_KEY
 
 # https://stackoverflow.com/questions/15512239/python-get-all-youtube-video-urls-of-a-channel
 class GetVideoList(Step):
-    def process(self, data, inputs):
+    def process(self, data, inputs, utils):
         channel_id = inputs['channel_id']
+
+        if utils.video_list_file_exists(channel_id):
+            print('found existing video list file for channel id', channel_id)
+            return self.read_file(utils.get_video_list_filepath(channel_id))
+
         base_video_url = 'https://www.youtube.com/watch?v='
         base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
 
         first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(API_KEY,
-                                                                                                                channel_id)
+                                                                                                            channel_id)
 
         video_links = []
         url = first_url
@@ -32,6 +37,17 @@ class GetVideoList(Step):
                 break
         print(video_links)
         print(len(video_links))
+        self.write_to_file(video_links, utils.get_video_list_filepath(channel_id))
         return video_links
 
+    def write_to_file(self, video_links, filepath):
+        with open(filepath, 'w') as f:
+            for url in video_links:
+                f.write(url + '\n')
 
+    def read_file(self, filepath):
+        video_links = []
+        with open(filepath, 'r') as f:
+            for url in f:
+                video_links.append(url.strip())
+        return video_links
