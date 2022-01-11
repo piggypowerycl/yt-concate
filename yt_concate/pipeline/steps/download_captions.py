e05f7c1fe6f9be9b3e37f6ea2d688090ea0fec99
piggypowerycl <yichulai@gmail.com>
2022-01-11 17:21:16 +0800

    Rewrote download_captions with multi-threading

--- yt_concate/pipeline/steps/download_captions.py
+++ yt_concate/pipeline/steps/download_captions.py
@@ -1,3 +1,5 @@
+from concurrent import futures
+
 from pytube import YouTube
 import time
 
@@ -7,23 +9,31 @@ from .step import Step
 class DownloadCaptions(Step):
     def process(self, data, inputs, utils):
         start = time.time()
-        for yt in data:
-            print('downloading caption for', yt.id)
-            if utils.caption_file_exists(yt):
-                print('found existing caption file')
-                continue
-            source = YouTube(yt.url)
-            try:
-                en_caption = source.captions.get_by_language_code('a.en')
-                en_caption_convert_to_srt = (en_caption.generate_srt_captions())
-            except (AttributeError, KeyError) as e:
-                print(e)
-                continue
 
-            text_file = open(utils.get_caption_filepath(yt.url), "w", encoding='utf-8')
-            text_file.write(en_caption_convert_to_srt)
-            text_file.close()
-
+        with futures.ThreadPoolExecutor(max_workers=30) as executor:
+            for yt in data:
+                if utils.caption_file_exists(yt):
+                    print('found existing caption file')
+                    continue
+                print('downloading caption for', yt.id)
+                try:
+                    executor.submit(self.download_captions, yt)
+                except (AttributeError, KeyError, TypeError) as e:
+                    print(e)
+                    continue
         end = time.time()
+
         print('took', end-start, 'seconds')
         return data
+
+    def download_captions(self, yt):
+        source = YouTube(yt.url)
+        try:
+            en_caption = source.captions.get_by_language_code('a.en')
+            en_caption_convert_to_srt = (en_caption.generate_srt_captions())
+        except (AttributeError, KeyError) as e:
+            return e
+
+        text_file = open(yt.caption_filepath, "w", encoding='utf-8')
+        text_file.write(en_caption_convert_to_srt)
+        text_file.close()
